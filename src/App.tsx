2d0f640f8ffec42f818b66cb2230ea75f00b8dbb
Dmitriy Yuganuk <dmitriyyuganuk@gmail.com>
2022-03-31 13:19:54 +0300

    samurai-way_12-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/header/Header";
+import {Navbar} from "./components/navbar/Navbar";
+import {Profile} from "./components/profile/Profile";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+function App () {
+    return (
+        <div className="app-wrapper">
+           <Header/>
+            <Navbar/>
+            <Profile/>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css"
+
+export function Header () {
+    return (
+        <header className="header">
+            <img src="https://seeklogo.com/images/B/business-company-logo-C561B48365-seeklogo.com.png" alt=" "/>
+        </header>
+    )
+}
